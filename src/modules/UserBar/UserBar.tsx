c3a02d6804be6289d00ace8f0af44326f0a48976
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-06-14 21:47:46 +0200

    feat: add static buttons
    notification button & add freight button. Removed user name

--- src/modules/UserBar/UserBar.tsx
+++ src/modules/UserBar/UserBar.tsx
@@ -1,14 +1,22 @@
 import { Avatar } from '@mui/material';
 import styles from './UserBar.module.scss';
 import { GiHamburgerMenu } from 'react-icons/gi';
+import { IoMdNotificationsOutline } from 'react-icons/io';
 export const UserBar = ({ setShowMenu }) => {
   return (
     <div className={styles.container}>
-      <button className={styles.button} onClick={() => setShowMenu((prevValue) => !prevValue)}>
+      <button
+        className={styles['hamburger-button']}
+        onClick={() => setShowMenu((prevValue) => !prevValue)}
+      >
         <GiHamburgerMenu />
       </button>
-      <p>Mateusz Kluska</p>
+      <button className={styles['freight-button']}>Add Freight</button>
+      <button className={styles['notification-button']}>
+        <IoMdNotificationsOutline />
+      </button>
       <Avatar
+        sx={{ width: 30, height: 30, cursor: 'pointer' }}
         alt={`Mateusz photo`}
         src='https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80'
       />
