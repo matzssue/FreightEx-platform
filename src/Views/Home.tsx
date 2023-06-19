2a8d193478d8f8736d87c633d12fd777134a20ef
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-06-19 22:30:26 +0200

    feat: first init
    main page to render all components

---
+++ src/Views/Home.tsx
@@ -0,0 +1,17 @@
+import { FilterDetails } from '../modules/Loads/ActiveFilters/components/FiltersDetails';
+import { FiltersMenu } from '../modules/Loads/LoadFilters/components/FiltersMenu';
+import { ActiveFilters } from '../modules/Loads/ActiveFilters/components/ActiveFilters';
+import { Loads } from '../modules/Loads/Loads/components/Loads';
+import { AllNews } from '../modules/News/NewsCard/components/AllNews';
+import { AddLoad } from '../modules/Loads/Loads/components/AddLoad';
+
+export const Home = () => {
+  return (
+    <>
+      <FiltersMenu />
+      <FilterDetails />
+      <ActiveFilters />
+      <Loads />
+    </>
+  );
+};
