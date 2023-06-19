d2dcf78256079cc12294f272f5ffbc2ed569ee4b
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-06-19 22:32:11 +0200

    feat: add Wrapper, Routes to Home and News pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,33 @@
-import { AsideMenu } from "./modules/AsideMenu/components/AsideMenu";
-import { UserBar } from "./modules/UserBar/UserBar";
-import styles from "./App.module.scss";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Loads from "./modules/Loads";
+import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import styles from './App.module.scss';
+import Wrapper from './common/Wrapper';
+import { AsideMenu } from './modules/Home/AsideMenu/components/AsideMenu';
+import { UserBar } from './modules/UserBar/components/UserBar';
+import { FiltersMenu } from './modules/Loads/LoadFilters/components/FiltersMenu';
+import { ActiveFilters } from './modules/Loads/ActiveFilters/components/ActiveFilters';
+import { Loads } from './modules/Loads/Loads/components/Loads';
+import { FilterDetails } from './modules/Loads/ActiveFilters/components/FiltersDetails';
+import { NewsCard } from './modules/News/NewsCard/components/NewsCard';
+import { AllNews } from './modules/News/NewsCard/components/AllNews';
+import { Home } from './Views/Home';
+import { News } from './Views/News';
+
 function App() {
+  const [showMenu, setShowMenu] = useState<boolean>(true);
+
   return (
     <>
       <BrowserRouter>
-        <UserBar />
+        <UserBar setShowMenu={setShowMenu} />
         <div className={styles.box}>
-          <AsideMenu />
-
-          <Routes>
-            <Route element={<Loads />} path="/loads" />
-          </Routes>
+          <AsideMenu showMenu={showMenu} />
+          <Wrapper showMenu={showMenu}>
+            <Routes>
+              <Route element={<Home />} path='/loads' />
+              <Route element={<News />} path='/news' />
+            </Routes>
+          </Wrapper>
         </div>
       </BrowserRouter>
     </>
