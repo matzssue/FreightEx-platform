874072e761d9eb40ab5927c02db92442cebfbe50
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-06-24 23:37:46 +0200

    fix: now works properly with useHookForm and yup

--- src/common/Inputs/DateInput.tsx
+++ src/common/Inputs/DateInput.tsx
@@ -4,22 +4,26 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import dayjs from 'dayjs';
+import { useState } from 'react';
 
 export const DateInput = ({ control, name, label = '' }) => {
   const currentDate = dayjs(new Date());
-  console.log(currentDate);
-
+  // console.log(currentDate);
+  const [date, setDate] = useState(currentDate);
   return (
     <Controller
       name={name}
       control={control}
+      defaultValue={currentDate}
       render={({ field, fieldState: { error } }) => (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
           <div className={styles.container}>
             {/* <label>Date</label> */}
             <DatePicker
               {...field}
-              value={currentDate}
+              onChange={(date) => setDate(date)}
+              className={styles.input}
+              value={date}
               label={label}
               slotProps={{
                 textField: {
--- src/common/Inputs/TextField.tsx
+++ src/common/Inputs/TextField.tsx
@@ -2,35 +2,33 @@ import { Controller } from 'react-hook-form';
 import TextField from '@mui/material/TextField';
 import { Control } from 'react-hook-form';
 import styles from './TextField.module.scss';
+import { useState } from 'react';
 type FormInputProps = {
   name: string;
   control: Control;
   label: string | null;
 };
 
-export const TextFieldInput = ({ name, control, label }: FormInputProps) => {
+export const TextFieldInput = ({ name, control, label, ...props }: FormInputProps) => {
   return (
     <Controller
       name={name}
       control={control}
+      defaultValue={''}
       render={({ field: { onChange, value }, fieldState: { error }, formState }) => (
         <div className={styles.container}>
-          <label>{label}</label>
+          <label className={styles.label}>{label}</label>
           <TextField
             helperText={error ? error.message : null}
             size='small'
             error={!!error}
             onChange={onChange}
             value={value}
-            fullWidth
+            // fullWidth
+
             label={null}
-            variant='outlined'
-            sx={{ width: 70 }}
-            inputProps={{
-              style: {
-                height: '10px',
-              },
-            }}
+            variant='standard'
+            {...props}
           />
         </div>
       )}
