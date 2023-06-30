06e2ce6878d5443e15f1054cb569b6b08f5dd336
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-06-30 23:17:58 +0200

    feat: data change from static to loads from supabase

--- src/modules/Loads/Loads/components/Loads.tsx
+++ src/modules/Loads/Loads/components/Loads.tsx
@@ -3,21 +3,39 @@ import { List } from '../../../../common/Lists/List';
 import { useEffect } from 'react';
 import { sortData } from '../loadData';
 import { Load } from './Load';
-
+import { getAllLoads } from '../../../../utils/api/supabase/load';
+import { useQuery } from '@tanstack/react-query';
 export const Loads = () => {
+  const { data, isLoading } = useQuery(['orders'], async () => await getAllLoads());
+  console.log(data);
   return (
     <div>
       <div className={styles['sort-list']}>
         <List list={sortData} />
       </div>
       <div className={styles.loads}>
+        {isLoading ? (
+          <p>loading</p>
+        ) : (
+          data?.map((load) => (
+            <Load
+              key={load.id}
+              loadingCity={load.loadingAddress.city}
+              loadingPostCode={load.loadingAddress.postal_code}
+              unloadingCity={load.unloadingAddress.city}
+              unloadingPostCode={load.unloadingAddress.postal_code}
+              {...load}
+            />
+          ))
+        )}
+        {/*       
         <Load />
         <Load />
         <Load />
         <Load />
         <Load />
         <Load />
-        <Load />
+        <Load /> */}
       </div>
     </div>
   );
