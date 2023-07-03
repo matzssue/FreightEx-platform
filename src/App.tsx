2a93e453aa5382002b5f8067c9c30b671a63ecb2
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-03 20:53:35 +0200

    feat: add suspense, remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
-import { useState, useEffect } from 'react';
+import { useState, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import styles from './App.module.scss';
 import Wrapper from './common/Wrapper';
 import { AsideMenu } from './modules/Home/AsideMenu/components/AsideMenu';
 import { UserBar } from './modules/UserBar/components/UserBar';
-import { FiltersMenu } from './modules/Loads/LoadFilters/components/FiltersMenu';
-import { ActiveFilters } from './modules/Loads/ActiveFilters/components/ActiveFilters';
-import { Loads } from './modules/Loads/Loads/components/Loads';
-import { FilterDetails } from './modules/Loads/ActiveFilters/components/FiltersDetails';
-import { NewsCard } from './modules/News/NewsCard/components/NewsCard';
-import { AllNews } from './modules/News/NewsCard/components/AllNews';
+
 import { Home } from './Views/Home';
 import { News } from './Views/News';
 import { Loader } from '@googlemaps/js-api-loader';
@@ -48,18 +43,20 @@ function App() {
         {process.env.NODE_ENV === 'development' && (
           <ReactQueryDevtools position='top-right' initialIsOpen={false} />
         )}
-        <BrowserRouter>
-          <UserBar setShowMenu={setShowMenu} />
-          <div className={styles.box}>
-            <AsideMenu showMenu={showMenu} />
-            <Wrapper showMenu={showMenu}>
-              <Routes>
-                <Route element={<Home />} path='/loads' />
-                <Route element={<News />} path='/news' />
-              </Routes>
-            </Wrapper>
-          </div>
-        </BrowserRouter>
+        <Suspense fallback='Loading...'>
+          <BrowserRouter>
+            <UserBar setShowMenu={setShowMenu} />
+            <div className={styles.box}>
+              <AsideMenu showMenu={showMenu} />
+              <Wrapper showMenu={showMenu}>
+                <Routes>
+                  <Route element={<Home />} path='/loads' />
+                  <Route element={<News />} path='/news' />
+                </Routes>
+              </Wrapper>
+            </div>
+          </BrowserRouter>
+        </Suspense>
       </QueryClientProvider>
     </>
   );
