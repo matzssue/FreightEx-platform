37d1cd94d1d7a508a931798ea2f35175ed8e16fe
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-08 21:14:32 +0200

    feat: remove min, max labels

--- src/modules/Loads/LoadFilters/components/FilterCard.tsx
+++ src/modules/Loads/LoadFilters/components/FilterCard.tsx
@@ -4,16 +4,14 @@ type FilterCard = {
   children: ReactNode;
   filterName: string;
   topLeftLabel: string;
-  topRightLabel: string;
+  topRightLabel?: string;
 };
 
 export const FilterCard = ({ children, filterName, topLeftLabel, topRightLabel }: FilterCard) => {
   return (
     <div className={styles.filter}>
       <span className={styles['filter-name']}>{filterName}</span>
-      <span className={styles.min}>{topLeftLabel}</span>
-      <span className={styles.max}>{topRightLabel}</span>
-      <div className={styles.inputs}>{children}</div>
+      <div>{children}</div>
     </div>
   );
 };
