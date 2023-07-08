391ed3319dd0c4624bddc499077cb15e41146cff
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-08 21:27:36 +0200

    feat: add supabase function to fetch filtered loads

--- src/utils/api/supabase/load.ts
+++ src/utils/api/supabase/load.ts
@@ -1,6 +1,10 @@
 import supabase from '../../../config/supabase';
 import { AddLoadValues } from '../../schemas/addLoadSchema';
 import { Database } from '../../../types/supabase';
+import { useAppSelector } from '../../../store/hooks';
+import { LoadsFiltersValues } from '../../schemas/loadsFilters';
+import moment from 'moment';
+import { toRadians } from '../../helpers/getBoundRadius';
 
 // export const addLoad = async (newLoad: AddLoadValues) => {
 //   const { data, error } = await supabase.from('loads').insert(newLoad);
@@ -68,7 +72,7 @@ export const addLoad = async (data: AddLoadData) => {
     vehicle_types: data.multiCheckbox,
   };
 
-  const { data: load, error: loadError } = await supabase.from('orders').insert(loadData);
+  const { data: load, error: loadError } = await supabase.from('loads').insert(loadData);
   console.log(loadError, load);
   if (error) throw new Error();
   return data;
@@ -98,6 +102,104 @@ export const getAllLoads = async () => {
     };
   });
   // console.log(data);
+
+  return loads;
+};
+export const getFilteredLoads = async (filter: LoadsFiltersValues) => {
+  if (!filter) return;
+  const {
+    maxWeight,
+    minLength,
+    minWeight,
+    maxLength,
+    startLoadingDate,
+    endLoadingDate,
+    startUnloadingDate,
+    endUnloadingDate,
+    loadingArea,
+    loadingPlaceData,
+    unloadingPlace,
+  } = filter;
+
+  let query;
+
+  if (!loadingArea) {
+    query = supabase.from('loads').select(`*, unloading_address_id(*), loading_address_id(*)`);
+    console.log('noLoadingArea', query);
+  }
+  if (loadingArea) {
+    // console.log(loadingPlaceData.latitude, loadingPlaceData.longitude, loadingArea);
+
+    query = supabase.rpc('get_entries_within_distance', {
+      distance: loadingArea,
+      tlatitude: loadingPlaceData.latitude,
+      tlongitude: loadingPlaceData.longitude,
+    });
+    console.log('loadingArea', query);
+  }
+
+  if (!query) return;
+
+  if (maxWeight) {
+    query = query.lte('weight', maxWeight);
+  }
+  if (minWeight) {
+    query = query.gte('weight', minWeight);
+  }
+  if (maxLength) {
+    query = query.lte('length', maxLength);
+  }
+  if (minLength) {
+    query = query.gte('length', minLength);
+  }
+  if (startLoadingDate) {
+    query = query.gte('loading_date', startLoadingDate);
+  }
+  if (endLoadingDate) {
+    query = query.lte('loading_date', endLoadingDate);
+  }
+  if (startUnloadingDate) {
+    query = query.gte('unloading_date', startUnloadingDate);
+  }
+  if (endUnloadingDate) {
+    query = query.lte('unloading_date', endUnloadingDate);
+  }
+
+  const { data, error } = await query;
+
+  if (error) throw new Error();
+  console.log(data);
+
+  // if (loadingArea) {
+  //   query = query.rpc('get_entries_within_distance', {
+  //     tlongitude: loadingPlace.longitude,
+  //     tlatitude: loadingPlace.latitude,
+  //     distance: loadingArea,
+  //   });
+  // }
+
+  // .order('created_at', { ascending: false })
+  // .returns<LoadData[]>();
+
+  // if (error) throw new Error();
+  // console.log(data);
+  // console.log(error);
+
+  const loads = data.map((load) => {
+    return {
+      id: load.id,
+      loadingAddress: load.loading_address_id,
+      unloadingAddress: load.unloading_address_id,
+      loadingDate: load.loading_date,
+      unloadingDate: load.unloading_date,
+      vehicleTypes: load.vehicle_types,
+      cargoLength: load.length,
+      cargoWeight: load.weight,
+      paymentTerm: load.term,
+      price: load.price,
+      currency: load.currency,
+    };
+  });
 
   return loads;
 };
