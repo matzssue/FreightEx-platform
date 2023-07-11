c6a56986d60f969e818666771c87d8953deca7c8
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-11 21:36:36 +0200

    feat: remove ThemeProvider from MUI
    no need to use this theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import './styles/global.scss';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './styles/themes.ts';
+
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
