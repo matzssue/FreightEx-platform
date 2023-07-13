21755b74043886c0a9258ab2962d6e560c561e9f
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-13 22:26:20 +0200

    feat: add generic LoadAddress component
    component displays info about country,postCode, city

---
+++ src/modules/Loads/LoadsList/components/Loads/LoadAddress.tsx
@@ -0,0 +1,18 @@
+import styles from './LoadAddress.module.scss';
+import { memo } from 'react';
+
+type LoadAddress = {
+  country: string;
+  postCode: string;
+  city: string;
+};
+export const LoadAddress = memo(({ country, postCode, city }: LoadAddress) => {
+  return (
+    <span>
+      <p className={styles.country}>{country}</p>
+      <p className={styles['post-code']}>
+        {postCode} {city}
+      </p>
+    </span>
+  );
+});
