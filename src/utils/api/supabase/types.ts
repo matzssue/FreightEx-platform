ec5268d73d2f37346502ab63210c310cba4c1e56
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-13 22:25:13 +0200

    feat: types transferred from 'loads.ts'

---
+++ src/utils/api/supabase/types.ts
@@ -0,0 +1,17 @@
+import { Database } from '../../../types/supabase';
+import { AddLoadValues } from '../../schemas/addLoadSchema';
+
+type AddressesDatabase = Database['public']['Tables']['addresses']['Row'];
+type Loads = Database['public']['Tables']['loads']['Row'];
+
+export type Addresses = Omit<AddressesDatabase, 'id'>;
+
+export type LoadData = Loads & {
+  unloading_address_id: AddressesDatabase;
+  loading_address_id: AddressesDatabase;
+};
+
+export type AddLoadData = AddLoadValues & {
+  loadingAddressData: Addresses;
+  unloadingAddressData: Addresses;
+};
