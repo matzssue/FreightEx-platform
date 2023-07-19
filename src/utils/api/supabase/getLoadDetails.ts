06f938cfca60617269ea33c5ee6d77fd9cc0c043
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-19 19:08:08 +0200

    feat: change name of load properties

--- src/utils/api/supabase/getLoadDetails.ts
+++ src/utils/api/supabase/getLoadDetails.ts
@@ -12,14 +12,14 @@ export const getLoadDetails = async (id: string) => {
   const loads = data.map((load) => {
     return {
       id: load.id,
-      loadingAddress: load.loading_address_id,
-      unloadingAddress: load.unloading_address_id,
+      loadingAddressData: load.loading_address_id,
+      unloadingAddressData: load.unloading_address_id,
       loadingDate: load.loading_date,
       unloadingDate: load.unloading_date,
       vehicleTypes: load.vehicle_types,
       cargoLength: load.length,
       cargoWeight: load.weight,
-      paymentTerm: load.term,
+      term: load.term,
       price: load.price,
       currency: load.currency,
     };
