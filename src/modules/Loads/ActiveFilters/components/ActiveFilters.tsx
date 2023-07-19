c04c335fe530dfce16f0e0349db274498729f7fd
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-19 17:02:06 +0200

    feat: add button to delete filter

--- src/modules/Loads/ActiveFilters/components/ActiveFilters.tsx
+++ src/modules/Loads/ActiveFilters/components/ActiveFilters.tsx
@@ -1,16 +1,25 @@
 import { Link } from 'react-router-dom';
 import styles from './ActiveFilters.module.scss';
 import { useAppSelector } from '../../../../store/hooks';
+import Links from '../../../../common/Lists/Links';
+import { IoCloseCircleSharp } from 'react-icons/io5';
 export const ActiveFilters = () => {
   const filters = useAppSelector((state) => state.loadsFilters.filters);
-  console.log(filters);
+  const deleteFilterHandler = () => {};
   return (
     <div className={styles['list-container']}>
-      {filters.map((filter, i) => (
-        <Link key={i} to={`/loads/${filter.id}`}>
-          {filter.loadingAddressData.city}
-        </Link>
-      ))}
+      <ul>
+        {filters.map((filter, i) => (
+          <li>
+            <Link key={i} to={`/loads/filters/${filter.id}`}>
+              {filter.loadingAddressData.city}
+            </Link>
+            <button onClick={deleteFilterHandler} className={styles.close}>
+              <IoCloseCircleSharp />
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
