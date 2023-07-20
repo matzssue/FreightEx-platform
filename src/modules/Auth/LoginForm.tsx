6fde3eafebe39d7aed448888ca856c8bbbe1d82c
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-20 21:50:10 +0200

    feat: add form, mutation on submit

--- src/modules/Auth/LoginForm.tsx
+++ src/modules/Auth/LoginForm.tsx
@@ -6,33 +6,42 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import { LoginFormValues, loginSchema } from '../../utils/schemas/authSchema';
 import { Link } from 'react-router-dom';
 import { AuthFormWrapper } from '../../common/AuthFormWrapper';
+import { useLogin } from '../../hooks/useLogin';
+import supabase from '../../config/supabase';
+import { useEffect } from 'react';
 export const LoginForm = () => {
+  const loginMutation = useLogin();
+
   const { handleSubmit, control } = useForm({
     resolver: yupResolver<LoginFormValues>(loginSchema),
   });
   const onSubmit = async (data: LoginFormValues) => {
     console.log(data);
+    loginMutation.mutateAsync(data);
+    console.log(data);
   };
 
   return (
-    <AuthFormWrapper onSubmit={handleSubmit(onSubmit)}>
-      <TextFieldInput
-        size='medium'
-        defaultValue={''}
-        label={'Email'}
-        control={control}
-        name='email'
-        row={true}
-      />
-      <PasswordInput label={'Password'} size='medium' control={control} name='password' />
-      <div className={styles.buttons}>
-        <button type='submit' className={styles.submit}>
-          Submit
-        </button>
-        <p className={styles.register}>
-          New to our platform? <Link to={'/register'}>Sign Up</Link>
-        </p>
-      </div>
+    <AuthFormWrapper>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextFieldInput
+          size='medium'
+          defaultValue={''}
+          label={'Email'}
+          control={control}
+          name='email'
+          row={true}
+        />
+        <PasswordInput label={'Password'} size='medium' control={control} name='password' />
+        <div className={styles.buttons}>
+          <button type='submit' className={styles.submit}>
+            Submit
+          </button>
+          <p className={styles.register}>
+            New to our platform? <Link to={'/register'}>Sign Up</Link>
+          </p>
+        </div>
+      </form>
     </AuthFormWrapper>
   );
 };
