0b040126f5e20e769a942d5b5efe310015622477
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-20 21:54:57 +0200

    feat: remove auth wrapper, form. Add FormContext

--- src/modules/Auth/CompanyStep.tsx
+++ src/modules/Auth/CompanyStep.tsx
@@ -1,5 +1,5 @@
 import styles from './CompanyStep.module.scss';
-import { useForm } from 'react-hook-form';
+import { useForm, useFormContext } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { RegisterCompanyFormValues, registerCompanySchema } from '../../utils/schemas/authSchema';
 import { AuthFormWrapper } from '../../common/AuthFormWrapper';
@@ -7,19 +7,9 @@ import { TextFieldInput } from '../../common/Inputs/TextFieldInput';
 import { useDispatch } from 'react-redux';
 import { nextStep, prevStep, setData } from '../../store/reducers/registerUserSlice';
 export const CompanyStep = () => {
-  const dispatch = useDispatch();
-
-  const { handleSubmit, control } = useForm({
-    resolver: yupResolver<RegisterCompanyFormValues>(registerCompanySchema),
-  });
-
-  const onSubmit = (data: RegisterCompanyFormValues) => {
-    dispatch(setData({ entityType: 'company', data }));
-    dispatch(nextStep());
-  };
-
+  const { control } = useFormContext();
   return (
-    <AuthFormWrapper hideLogo={true} onSubmit={handleSubmit(onSubmit)}>
+    <>
       <TextFieldInput
         defaultValue={''}
         label={'Company name'}
@@ -28,15 +18,6 @@ export const CompanyStep = () => {
         row={true}
       />
       <TextFieldInput defaultValue={''} label={'VATID'} control={control} name='vatId' row={true} />
-
-      <div className={styles.buttons}>
-        <button type='button' onClick={() => dispatch(prevStep())} className={styles.back}>
-          Back
-        </button>
-        <button type='submit' className={styles.submit}>
-          Register
-        </button>
-      </div>
-    </AuthFormWrapper>
+    </>
   );
 };
