e7b7f05139dfac1cb9f1fee02e1c9dd888401e33
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-07-28 18:34:52 +0200

    feat: change name of vehicles

--- src/modules/Loads/LoadsList/components/AddLoad/VehicleDetails.tsx
+++ src/modules/Loads/LoadsList/components/AddLoad/VehicleDetails.tsx
@@ -12,21 +12,19 @@ export const VehicleDetails = ({
   return (
     <div className={styles['vehicle-inputs']}>
       <Title title={'Vehicle'} />
-      <CheckboxInput<AddLoadValues> control={control} label={'solo'} name={'multiCheckbox.solo'} />
-      <CheckboxInput<AddLoadValues> control={control} label={'bus'} name={'multiCheckbox.bus'} />
+      <CheckboxInput<AddLoadValues> control={control} label={'solo'} name={'vehicles.solo'} />
+      <CheckboxInput<AddLoadValues> control={control} label={'bus'} name={'vehicles.bus'} />
       <CheckboxInput<AddLoadValues>
         control={control}
         label={'with semi-trailer'}
-        name={'multiCheckbox.semiTrailer'}
+        name={'vehicles.semi trailer'}
       />
       <CheckboxInput<AddLoadValues>
         control={control}
         label={'with double-trailer'}
-        name={'multiCheckbox.doubleTrailer'}
+        name={'vehicles.double trailer'}
       />
-      {errors?.multiCheckbox && (
-        <p className={styles.error}>{`${errors?.multiCheckbox?.message}`}</p>
-      )}
+      {errors?.vehicles && <p className={styles.error}>{`${errors?.vehicles?.message}`}</p>}
     </div>
   );
 };
