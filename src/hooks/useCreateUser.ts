265c8450a4768aaf0c2159098343aa4b253f81f5
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-04 22:03:54 +0200

    feat: remove avatar from user data
    avatar is set to default image for avatars in supabase

--- src/hooks/useCreateUser.ts
+++ src/hooks/useCreateUser.ts
@@ -24,8 +24,6 @@ const createUser = async (user: UserData) => {
 export default function useCreateUser() {
   //   const { setUser } = useUserContext();
   //   const notificationCtx = useNotificationContext();
-  const standardAvatar =
-    'https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80';
 
   const navigate = useNavigate();
 
@@ -43,11 +41,10 @@ export default function useCreateUser() {
         name: user.name,
         surname: user.surname,
         email: user.email,
-        avatar: standardAvatar,
         company_vat_id: user.vatId,
       });
 
-      navigate('/');
+      navigate('/login');
 
       if (insertError) {
         throw insertError;
