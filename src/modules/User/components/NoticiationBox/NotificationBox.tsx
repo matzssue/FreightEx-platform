84c18787ab448c65c70dcb3f64c488296be58cb6
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-20 16:31:24 +0200

    feat: remove function to save notifications to localstorage

--- src/modules/User/components/NoticiationBox/NotificationBox.tsx
+++ src/modules/User/components/NoticiationBox/NotificationBox.tsx
@@ -1,51 +1,29 @@
 import styles from './NotificaitionBox.module.scss';
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode, useState } from 'react';
 import { IoMdNotificationsOutline } from 'react-icons/io';
-import {
-  NotificationCenterItem,
-  useNotificationCenter,
-} from 'react-toastify/addons/use-notification-center';
-import { toast } from 'react-toastify';
+import { useNotificationCenter } from 'react-toastify/addons/use-notification-center';
 import { Badge } from '@mui/material';
 
 import { AiOutlineClose } from 'react-icons/ai';
 import { Title } from '../../../../common/Title/Title';
 import { BsPatchCheck, BsPatchCheckFill } from 'react-icons/bs';
 export const NotificationBox = () => {
-  const { notifications, clear, markAllAsRead, markAsRead } = useNotificationCenter();
+  const { notifications, clear, markAllAsRead, markAsRead, remove, unreadCount } =
+    useNotificationCenter();
   const [isOpen, setIsOpen] = useState(false);
-  const [savedNotifications, setSavedNotifications] = useState<NotificationCenterItem[] | null>([]);
+
   const toggleNotificationBox = () => {
     setIsOpen((isOpen) => !isOpen);
   };
 
-  useEffect(() => {
-    const storedNotifications = localStorage.getItem('notifications');
-
-    if (storedNotifications) {
-      const parsedNotifications = JSON.parse(storedNotifications);
-      setSavedNotifications(parsedNotifications);
-    }
-  }, []);
-
-  useEffect(() => {
-    if (notifications.length <= 0) return;
-    localStorage.setItem('notifications', JSON.stringify(notifications));
-  }, [notifications]);
-
   const clearHandler = () => {
     clear();
-    setSavedNotifications(null);
-    localStorage.setItem('notifications', JSON.stringify([]));
   };
-
-  const selectNotifications =
-    savedNotifications && savedNotifications.length > 0 ? savedNotifications : notifications;
 
   return (
     <div className={styles['notifications-box']}>
       <button onClick={toggleNotificationBox} className={styles['notification-button']}>
-        <Badge badgeContent={selectNotifications.length}>
+        <Badge badgeContent={unreadCount}>
           <IoMdNotificationsOutline />
         </Badge>
       </button>
@@ -53,21 +31,20 @@ export const NotificationBox = () => {
         <div className={styles['notifications-container']}>
           <Title title='Notifications' />
           <ul>
-            {selectNotifications.length > 0 ? (
-              selectNotifications.map((notification, i) => {
+            {notifications.length > 0 ? (
+              notifications.map((notification) => {
                 return (
-                  <li key={i} className={styles[notification.type as string]}>
-                    <span>{notification.content as ReactNode} </span>
-                    {notification.read ? (
-                      <BsPatchCheckFill />
-                    ) : (
-                      <button
-                        className={styles['mark-button']}
-                        onClick={() => markAsRead(notification.id)}
-                      >
-                        <BsPatchCheck />
-                      </button>
-                    )}
+                  <li key={notification.id} className={styles[notification.type as string]}>
+                    <button
+                      className={styles['notification']}
+                      onClick={() => markAsRead(notification.id)}
+                    >
+                      <span>{notification.content as ReactNode} </span>
+                      {notification.read ? <BsPatchCheckFill /> : <BsPatchCheck />}
+                    </button>
+                    <button className={styles.remove} onClick={() => remove(notification.id)}>
+                      X
+                    </button>
                   </li>
                 );
               })
@@ -76,7 +53,7 @@ export const NotificationBox = () => {
             )}
           </ul>
 
-          <button className={styles['read-all_button']} onClick={markAllAsRead}>
+          <button className={styles['read-all_button']} onClick={() => markAllAsRead()}>
             Read all
           </button>
           <button className={styles['clear-button']} onClick={() => clearHandler()}>
