79c92b2c6766491d32dcd9a30f1dbe30ef72ced3
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-20 16:58:39 +0200

    feat: add typescript

--- src/modules/Loads/components/Filters/LoadFilters/FilterInput/FilterInput.tsx
+++ src/modules/Loads/components/Filters/LoadFilters/FilterInput/FilterInput.tsx
@@ -1,6 +1,7 @@
+import { ReactNode } from 'react';
 import styles from './FilterInput.module.scss';
 
-export const FilterInput = ({ children, label }) => {
+export const FilterInput = ({ children, label }: { children: ReactNode; label: string }) => {
   return (
     <div className={styles['input-container']}>
       <label>{label}</label>
