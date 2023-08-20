47294268d87f5912706dcd5d7a82c270c3e6ac2c
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-20 16:38:26 +0200

    feat: add common props, remove legend and add label for inputs

--- src/modules/Loads/components/Filters/LoadFilters/DateFilter/DateFilter.tsx
+++ src/modules/Loads/components/Filters/LoadFilters/DateFilter/DateFilter.tsx
@@ -7,51 +7,29 @@ type DateFilterProps<T extends FieldValues> = {
   control: Control<T>;
 };
 
+const commonProps = {
+  fontSize: '15px',
+  size: 'small' as const,
+  sx: { boxShadow: '3px 3px 0px 0px rgba(148, 148, 148, 0.267)' },
+};
+
 export const DateFilter = <T extends FieldValues>({ control }: DateFilterProps<T>) => {
   return (
     <FilterCard filterName={'Date'}>
       <div className={styles['date-inputs']}>
         <fieldset className={styles['date-fieldset']}>
-          <legend>Loading</legend>
-          <span>Min</span>
-          <DateInput
-            fontSize='15px'
-            size='small'
-            control={control}
-            name={'startLoadingDate' as Path<T>}
-            label=''
-          />
-          <span>Max</span>
-          <DateInput
-            fontSize='15px'
-            size='small'
-            control={control}
-            name={'endLoadingDate' as Path<T>}
-            label=''
-          />
+          <label htmlFor='startLoadingDate'>Min loading date</label>
+          <DateInput {...commonProps} control={control} name={'startLoadingDate' as Path<T>} />
+          <label htmlFor='endLoadingDate'>Max loading date</label>
+          <DateInput {...commonProps} control={control} name={'endLoadingDate' as Path<T>} />
         </fieldset>
 
-        <div>
-          <fieldset className={styles['date-fieldset']}>
-            <legend>Unloading</legend>
-            <span>Min</span>
-            <DateInput
-              fontSize='15px'
-              size='small'
-              control={control}
-              name={'startUnloadingDate' as Path<T>}
-              label=''
-            />
-            <span>Max</span>
-            <DateInput
-              fontSize='15px'
-              size='small'
-              control={control}
-              name={'endUnloadingDate' as Path<T>}
-              label=''
-            />
-          </fieldset>
-        </div>
+        <fieldset className={styles['date-fieldset']}>
+          <label htmlFor='startUnloadingDate'>Min unloading date</label>
+          <DateInput {...commonProps} control={control} name={'startUnloadingDate' as Path<T>} />
+          <label htmlFor='endUnloadingDate'>Max unloading date</label>
+          <DateInput {...commonProps} control={control} name={'endUnloadingDate' as Path<T>} />
+        </fieldset>
       </div>
     </FilterCard>
   );
