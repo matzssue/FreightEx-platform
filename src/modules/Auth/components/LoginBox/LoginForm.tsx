1acd40e4a67ac2062f50f555a7c0d3fb17a3a4e9
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-23 18:49:52 +0200

    feat: add test account login  credencials

--- src/modules/Auth/components/LoginBox/LoginForm.tsx
+++ src/modules/Auth/components/LoginBox/LoginForm.tsx
@@ -61,8 +61,8 @@ export const LoginForm = () => {
         </div>
         <div className={styles['test-account']}>
           <h4>Test Acccount</h4>
-          <p>Login : test@transportex.com</p>
-          <p>Password : Test123</p>
+          <p>Login: test@transportex.com</p>
+          <p>Password: Test123</p>
         </div>
       </form>
     </AuthFormWrapper>
