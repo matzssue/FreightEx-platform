7122e96795566adb74b1b9f1560f37dc192ddc99
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-28 10:59:17 +0200

    feat: add function to get all published orders from supabase

---
+++ src/utils/api/supabase/Orders/getPublishedOrders.ts
@@ -0,0 +1,40 @@
+import supabase from 'src/config/supabase';
+import { Load, GetLoadsData } from '../types';
+export const getPublishedOrders = async (userId: string | undefined) => {
+  console.log(userId);
+  const { data: ordersData, error } = await supabase
+    .from('loads')
+    .select(`*, unloading_address_id(*), loading_address_id(*), user_id(*, company_vat_id(*))`)
+    .eq('user_id', userId)
+    .returns<GetLoadsData[]>();
+
+  if (error) throw new Error();
+  const orders = ordersData.map((load) => {
+    return {
+      id: load.id,
+      loadingAddress: load.loading_address_id,
+      unloadingAddress: load.unloading_address_id,
+      loadingDate: load.loading_date,
+      unloadingDate: load.unloading_date,
+      vehicleTypes: load.vehicle_types,
+      cargoLength: load.length,
+      cargoWeight: load.weight,
+      paymentTerm: load.term,
+      price: load.price,
+      currency: load.currency,
+      user: {
+        avatar: load.user_id.avatar,
+        email: load.user_id.email,
+        id: load.user_id.id,
+        name: load.user_id.name,
+        surname: load.user_id.surname,
+      },
+      company: load.user_id.company_vat_id,
+      createdAt: load.created_at,
+    };
+  }) as Load[];
+
+  console.log('ordersData,', ordersData);
+
+  return orders;
+};
