d2b655f5953583d67600a64bf88cb5eba0b0af6e
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-28 18:39:54 +0200

    feat: add types for accepted loads

--- src/utils/api/supabase/types.ts
+++ src/utils/api/supabase/types.ts
@@ -4,7 +4,7 @@ export type AddressesDatabase = Database['public']['Tables']['addresses']['Row']
 export type LoadsDatabase = Database['public']['Tables']['loads']['Row'];
 export type UserDatabase = Database['public']['Tables']['users']['Row'];
 export type CompanyDatabase = Database['public']['Tables']['companies']['Row'];
-export type AcceptedLoad = Database['public']['Tables']['accepted_loads']['Row'];
+export type AcceptedLoadDatabase = Database['public']['Tables']['accepted_loads']['Row'];
 export type InsertAcceptedLoad = Database['public']['Tables']['accepted_loads']['Insert'];
 export type InsertVehicle = Database['public']['Tables']['vehicles']['Insert'];
 export type UserDatabaseWithComp = {
@@ -39,7 +39,22 @@ export type GetLoadsData = {
   user_id: UserDatabaseWithComp;
   created_at: string;
 };
-
+export type GetAcceptedLoadsData = {
+  id: string;
+  loading_address_id: AddressesDatabase;
+  unloading_address_id: AddressesDatabase;
+  loading_date: string;
+  unloading_date: string;
+  vehicle_types: Vehicles;
+  length: number;
+  weight: number;
+  term: string;
+  price: string;
+  currency: string;
+  user_id: UserDatabaseWithComp;
+  accepted_by: UserDatabaseWithComp;
+  created_at: string;
+};
 export type AddLoadData = {
   currency: string;
   length?: number;
@@ -72,6 +87,22 @@ export type LoadsData = {
   created_at: string;
 };
 export type Load = {
+  id: string;
+  loadingAddress: AddressesDatabase;
+  unloadingAddress: AddressesDatabase;
+  loadingDate: string;
+  unloadingDate: string;
+  vehicleTypes: Vehicles;
+  cargoLength: number;
+  cargoWeight: number;
+  paymentTerm: string;
+  price: string;
+  currency: string;
+  user: UserDatabase;
+  company: CompanyDatabase;
+  createdAt: string;
+};
+export type AcceptedLoad = {
   id: string;
   loadingAddress: AddressesDatabase;
   unloadingAddress: AddressesDatabase;
