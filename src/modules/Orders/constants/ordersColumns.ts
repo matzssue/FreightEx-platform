7167de83e220f4451a8526f01442f83791a6dfb2
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-28 18:38:04 +0200

    feat: add consts with list of columns for orders

---
+++ src/modules/Orders/constants/ordersColumns.ts
@@ -0,0 +1,11 @@
+export const publishedOrdersColumns = ['Route', 'Salary', 'Cargo', 'Date', 'Options'];
+export const acceptedOrdersColumns = ['Route', 'Salary', 'Cargo', 'Date', 'Accepted by'];
+export const receivedOrdersColumns = [
+  'FV',
+  'Route',
+  'Salary',
+  'Cargo',
+  'Date',
+  'Publisher',
+  'Vehicle',
+];
