64cb84d89cd68bde74b40970433624f001b0571d
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-29 20:07:45 +0200

    feat: add notification clearing on logout

--- src/modules/Auth/hooks/useLogout.ts
+++ src/modules/Auth/hooks/useLogout.ts
@@ -3,9 +3,11 @@ import supabase from '../../../config/supabase';
 import { useUserContext } from '../../../store/contexts/UserContext';
 import { useNavigate } from 'react-router-dom';
 import { useQueryClient } from '@tanstack/react-query';
+import { useNotificationContext } from 'src/store/contexts/NotficationContext';
 export const useLogout = () => {
   const navigation = useNavigate();
   const queryClient = useQueryClient();
+  const { clear } = useNotificationContext();
   const { setIsLoggedIn, setUserId } = useUserContext();
   async function logout() {
     const { error } = await supabase.auth.signOut();
@@ -20,6 +22,7 @@ export const useLogout = () => {
       setIsLoggedIn(false);
       queryClient.clear();
       navigation('/login');
+      clear();
     },
     onError: (error) => {
       throw error;
