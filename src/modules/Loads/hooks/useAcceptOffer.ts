c5eccd610f67d0e5389e95d8af9fda46979441e0
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-08-29 21:36:05 +0200

    feat: add function to delete after its added to accepted loads

--- src/modules/Loads/hooks/useAcceptOffer.ts
+++ src/modules/Loads/hooks/useAcceptOffer.ts
@@ -1,38 +1,46 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import supabase from '../../../config/supabase';
 import { useNotificationContext } from 'src/store/contexts/NotficationContext';
-const addAcceptedLoad = async (loadId: string, userId: string) => {
-  if (!loadId || !userId) return;
-
-  const { data: loadData, error } = await supabase
-    .from('loads')
-    .select(`*`)
-    .eq('id', loadId)
-    .single();
-
-  if (error) throw new Error();
-
-  const { id, ...loadDataWithoutId } = loadData;
-  const loadWithUser = { ...loadDataWithoutId, accepted_by: userId };
-  if (!loadWithUser) return;
-  const { data, error: loadError } = await supabase.from('accepted_loads').insert(loadWithUser);
-  if (loadError) throw loadError;
-
-  return data;
-};
-
 export const useAcceptOffer = () => {
   const { notify } = useNotificationContext();
   const queryClient = useQueryClient();
+
+  const addAcceptedLoad = async (loadId: string, userId: string) => {
+    if (!loadId || !userId) return;
+
+    const { data: loadData, error } = await supabase
+      .from('loads')
+      .select(`*`)
+      .eq('id', loadId)
+      .single();
+
+    if (error) throw new Error();
+    if (!loadData) return;
+    const { id, ...loadDataWithoutId } = loadData;
+    const loadWithUser = { ...loadDataWithoutId, accepted_by: userId };
+    if (!loadWithUser) return;
+    const { data, error: loadError } = await supabase.from('accepted_loads').insert(loadWithUser);
+    if (loadError) throw loadError;
+
+    return data;
+  };
+
+  const deleteFromLoads = async (loadId: string) => {
+    if (!loadId) return;
+
+    const { error } = await supabase.from('loads').delete().eq('id', loadId);
+    if (error) throw error;
+  };
+
   return useMutation(
     ['loads'],
     async ({ loadId, userId }: { loadId: string; userId: string }) =>
       await addAcceptedLoad(loadId, userId),
     {
-      onSuccess: async (data, id) => {
-        console.log('succes id', data, id);
+      onSuccess: async (_, id) => {
         notify('success', 'Offer accepted');
-        queryClient.invalidateQueries(['received']);
+        await deleteFromLoads(id.loadId);
+        queryClient.invalidateQueries(['received', 'loads']);
       },
       onError: (error: { message: string }) => {
         console.log(error);
