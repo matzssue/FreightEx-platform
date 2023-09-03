54373a4cb3ba74a1562f3eb08995c023f2018e3a
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-03 20:48:03 +0200

    feat: change loading and no results information to work properly

--- src/modules/Loads/components/Load/LoadsList/LoadsList.tsx
+++ src/modules/Loads/components/Load/LoadsList/LoadsList.tsx
@@ -11,10 +11,11 @@ import { useUserContext } from '../../../../../store/contexts/UserContext';
 import { LoadingSpinner } from '../../../../../common/LoadingSpinner/LoadingSpinner';
 import { useAcceptOffer } from '../../../hooks/useAcceptOffer';
 import { Paginate } from '../../../../../common/Pagination/Pagination';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 import { useState } from 'react';
 import { Load as TLoad } from '../../../../../utils/api/supabase/types';
 import { useDeleteOrder } from 'src/modules/Orders/hooks/useDeleteOrder';
+import { usePaginationContext } from 'src/store/contexts/PaginationContext';
 export const Loads = () => {
   const { filterId } = useParams<string>();
   const acceptOfferMutation = useAcceptOffer();
@@ -24,25 +25,29 @@ export const Loads = () => {
 
   const filters = useAppSelector((state) => state.loadsFilters.filters);
 
-  const { data: allLoads } = useQuery(['loads'], async () => await getAllLoads());
+  const { data: allLoads, isLoading: isAllLoading } = useQuery(
+    ['loads'],
+    async () => await getAllLoads(),
+  );
 
   const fetchFilteredLoads = async () => {
-    if (!filters || !filterId) return [];
     const foundFilter = filters.find((filter) => filter.id === filterId);
     if (!foundFilter) return;
     return await getFilteredLoads(foundFilter);
   };
 
-  const { data: filteredLoads } = useQuery(['filteredLoads', filterId], fetchFilteredLoads, {
-    enabled: Boolean(filters && filterId),
-  });
+  const { data: filteredLoads, isLoading: isFilteredLoading } = useQuery(
+    ['filteredLoads', filterId],
+    fetchFilteredLoads,
+    {
+      enabled: Boolean(filters && filterId),
+    },
+  );
 
   const memoizedLoads = useMemo(
     () => (filterId ? filteredLoads : allLoads),
     [filterId, filteredLoads, allLoads],
   );
-
-  if (!userData) return;
 
   const acceptOfferHandler = async (e: React.MouseEvent<HTMLButtonElement>, id: string) => {
     e.stopPropagation();
@@ -53,7 +58,7 @@ export const Loads = () => {
     e.stopPropagation();
     deleteOfferMutation.mutate(id);
   };
-
+  if (isAllLoading || (filteredLoads && isFilteredLoading)) return <LoadingSpinner />;
   if (!memoizedLoads) return;
   return (
     <div className={styles['loads-container']}>
@@ -62,20 +67,16 @@ export const Loads = () => {
       </div>
       <div className={styles.loads}>
         <ul>
-          {!memoizedLoads ? (
-            <LoadingSpinner />
-          ) : (
-            memoizedLoads?.map((load) => (
-              <li id={`${load.id}`} key={load.id}>
-                <LoadCard
-                  onDelete={(e) => deleteOrderHandler(e, load.id)}
-                  onAccept={(e) => acceptOfferHandler(e, load.id)}
-                  data={load}
-                />
-              </li>
-            ))
-          )}
-          {slicedLoads && slicedLoads?.length <= 0 ? (
+          {memoizedLoads?.map((load) => (
+            <li id={`${load.id}`} key={load.id}>
+              <LoadCard
+                onDelete={(e) => deleteOrderHandler(e, load.id)}
+                onAccept={(e) => acceptOfferHandler(e, load.id)}
+                data={load}
+              />
+            </li>
+          ))}
+          {memoizedLoads && memoizedLoads?.length <= 0 ? (
             <p className={styles['no-results']}>No results found</p>
           ) : (
             ''
