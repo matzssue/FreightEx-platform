389a54eead656968a521f6da9b3d338be057dc9e
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-17 18:06:45 +0200

    feat: remove console logs

--- src/utils/api/supabase/Company/getCompany.ts
+++ src/utils/api/supabase/Company/getCompany.ts
@@ -1,14 +1,12 @@
 import supabase from '../../../../config/supabase';
 
 export const getCompany = async (id: string) => {
-  console.log(id);
   const { data: companyData, error } = await supabase
     .from('companies')
     .select('*')
     .eq('vat_id', id)
     .single();
   if (error) throw new Error();
-  console.log(companyData);
 
   return companyData;
 };
