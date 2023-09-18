524ff80c03bc32da213464b2b3e23f655949a81b
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-18 11:24:02 +0200

    feat: add invalidate queries, change update function to update all orders

--- src/modules/Invoices/hooks/useAddInvoice.ts
+++ src/modules/Invoices/hooks/useAddInvoice.ts
@@ -1,38 +1,54 @@
 import { useNotificationContext } from 'src/store/contexts/NotficationContext';
-import { useQueryClient, useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import supabase from 'src/config/supabase';
-
+import { InsertInvoice } from 'src/utils/api/supabase/types';
 export const useAddInvoince = () => {
   const { notify } = useNotificationContext();
   const queryClient = useQueryClient();
-
-  const addInvoice = async (invoiceData) => {
-    console.log(invoiceData);
-    const { load_id, ...invoiceDataWithoutId } = invoiceData;
+  const addInvoice = async (invoiceData: InsertInvoice, loadsId: string[]) => {
+    console.log(loadsId);
     const { data: invoice, error: loadError } = await supabase
       .from('invoices')
-      .insert(invoiceDataWithoutId)
+      .insert(invoiceData)
       .select()
       .single();
+
     if (loadError) throw loadError;
-    console.log(invoice);
+
     if (!invoice) return;
-    const { error: updateError } = await supabase
-      .from('accepted_loads')
-      .update({ invoice_id: invoice.id })
-      .eq('id', load_id)
-      .select();
-    console.log(updateError);
+
+    const updateLoads = async () => {
+      const promises = loadsId.map(async (id) => {
+        const { error: updateError } = await supabase
+          .from('accepted_loads')
+          .update({ invoice_id: invoice.id })
+          .eq('id', id)
+          .select();
+        return { id, updateError };
+      });
+
+      const results = await Promise.all(promises);
+
+      return results;
+    };
+
+    updateLoads();
   };
-  return useMutation(async (invoiceData) => await addInvoice(invoiceData), {
-    onSuccess: async () => {
-      notify('success', 'Invoice added');
-      //   queryClient.invalidateQueries(['loads']);
-      //   queryClient.invalidateQueries(['filteredLoads']);
+  return useMutation(
+    ['invoices'],
+    async ({ invoiceData, loadsId }: { invoiceData: InsertInvoice; loadsId: string[] }) =>
+      await addInvoice(invoiceData, loadsId),
+    {
+      onSuccess: async () => {
+        notify('success', 'Invoice added');
+        queryClient.invalidateQueries(['companyFactures']);
+        queryClient.invalidateQueries(['ordersToFacture']);
+        queryClient.invalidateQueries(['receivedOrders']);
+      },
+      onError: (error: { message: string }) => {
+        console.log(error);
+        notify('error', 'Something went wrong while accepting offer');
+      },
     },
-    onError: (error: { message: string }) => {
-      console.log(error);
-      notify('error', 'Something went wrong while accepting offer');
-    },
-  });
+  );
 };
