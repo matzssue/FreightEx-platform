aea3e497b1c734f00d18480a4471d9b1ba5571f5
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-21 22:20:36 +0200

    feat: add invoices list

--- src/modules/Invoices/components/InvoiceMenu/InvoiceMenu.tsx
+++ src/modules/Invoices/components/InvoiceMenu/InvoiceMenu.tsx
@@ -1,13 +1,16 @@
+import styles from './InvoiceMenu.module.scss';
+
 import { useState } from 'react';
 import { AddCollectiveInvoice } from '../AddInvoice/AddCollectiveInvoice/AddCollectiveInvoice';
 import { AddInvoice } from '../AddInvoice/AddSingleInvoice/AddInvoice';
+import { InvoiceList } from '../InvoiceList/InvoiceList';
 
 export const InvoiceMenu = () => {
   const [isSingleFactureModalOpen, setIsSingleFactureModalOpen] = useState<boolean>(false);
   const [isCollectiveFactureModalOpen, setIsCollectiveFactureModalOpen] = useState<boolean>(false);
 
   return (
-    <div>
+    <div className={styles.invoices}>
       <AddInvoice
         isModalOpen={isSingleFactureModalOpen}
         onClose={() => setIsSingleFactureModalOpen(false)}
@@ -16,8 +19,13 @@ export const InvoiceMenu = () => {
         isModalOpen={isCollectiveFactureModalOpen}
         onClose={() => setIsCollectiveFactureModalOpen(false)}
       />
-      <button onClick={() => setIsSingleFactureModalOpen(true)}>Add invoice</button>
-      <button onClick={() => setIsCollectiveFactureModalOpen(true)}>Add collective invoice</button>
+      <div className={styles['invoices-buttons']}>
+        <button onClick={() => setIsSingleFactureModalOpen(true)}>Add invoice</button>
+        <button onClick={() => setIsCollectiveFactureModalOpen(true)}>
+          Add collective invoice
+        </button>
+      </div>
+      <InvoiceList />
     </div>
   );
 };
