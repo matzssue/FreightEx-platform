9af7d523a255ef04bd14a9ab8b9d381bfa6d651e
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-23 14:39:17 +0200

    feat: add showJoyrideLoader hook

--- src/Pages/Orders/Orders.tsx
+++ src/Pages/Orders/Orders.tsx
@@ -2,13 +2,22 @@ import { OrdersMenu } from 'src/modules/Orders/components/OrdersMenu/OrdersMenu'
 import { ProtectedWrapper } from 'src/common/ProtectedWrapper/ProtectedWrapper';
 import { PageLayout } from 'src/common/PageLayout/PageLayout';
 import { OrdersWrapper } from 'src/modules/Orders/components/OrdersWrapper/OrdersWrapper';
+import { JoyrideLoader } from 'src/modules/Joyride/components/JoyrideLoader/JoyrideLoader';
+import { useMountJoyride } from 'src/modules/Joyride/hooks/useMountJoyride';
+import { SelectOrdersInformation } from 'src/modules/Orders/components/SelectOrdersInformation/SelectOrdersInformation';
+
+const joyrideIndex = 5;
+
 export const Orders = () => {
+  const { showJoyrideLoader } = useMountJoyride(joyrideIndex);
+
   return (
     <ProtectedWrapper>
       <PageLayout>
         <OrdersWrapper>
           <OrdersMenu />
-          <div>Please select orders</div>
+          {showJoyrideLoader && <JoyrideLoader />}
+          <SelectOrdersInformation />
         </OrdersWrapper>
       </PageLayout>
     </ProtectedWrapper>
