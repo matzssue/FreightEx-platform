8a0f9d7355338bebd7de3a097c628a27ac069ea9
Mateusz Kluska <mateuszkluska7@gmail.com>
2023-09-23 15:46:29 +0200

    feat: first init

---
+++ src/modules/Orders/components/SelectOrdersInformation/SelectOrdersInformation.tsx
@@ -0,0 +1,9 @@
+import styles from './SelectOrdersInformation.module.scss';
+
+export const SelectOrdersInformation = () => {
+  return (
+    <div className={styles['orders-information']}>
+      <p>Please select the type of orders</p>
+    </div>
+  );
+};
